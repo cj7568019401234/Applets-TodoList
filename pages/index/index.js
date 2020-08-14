d881330ba05209bce03a303c2a4ea94feb5d4e63
liucaijun <liucaijun@jd.com>
2020-08-14 16:58:44 +0800

    fix data onload

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,55 +4,57 @@ const app = getApp()
 
 Page({
   data: {
-    todoList: [{
-      id: 1,
-      value: 'hahaha todoList开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发',
-      endDate: '2020年10月12日',
-      endTime: '20:18',
-      isFinished: false
-    }, {
-      id: 2,
-      value: '这是第二个任务啦',
-      endDate: '2020年10月12日',
-      endTime: '20:18',
-      isFinished: true
-    }, {
-      id: 3,
-      value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-      endDate: '2020年10月16日',
-      endTime: '23:48',
-      isFinished: false
-    }, {
-      id: 4,
-      value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-      endDate: '2020年10月16日',
-      endTime: '',
-      isFinished: false
-    }, {
-      id: 5,
-      value: '任务6ixixi',
-      endDate: '',
-      endTime: '',
-      isFinished: false
-    }, {
-      id: 5,
-      value: '任务6ixixi',
-      endDate: '',
-      endTime: '23:17',
-      isFinished: false
-    }], //具体的待办事项
+    todoNum: 0,
+    doneNum: 0,
     todo: {
       isShowed: true, //是否展示
       spread: false, //是否点击展开
       collapse: false, //是否点击收缩
-      num: 0,
     },
     done: {
       isShowed: true,
       spread: false,
       collapse: false,
-      num: 0
-    }
+    },
+    todoList: [
+    //   {
+    //   id: 1,
+    //   value: 'hahaha todoList开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发',
+    //   endDate: '2020年10月12日',
+    //   endTime: '20:18',
+    //   isFinished: false
+    // }, {
+    //   id: 2,
+    //   value: '这是第二个任务啦',
+    //   endDate: '2020年10月12日',
+    //   endTime: '20:18',
+    //   isFinished: true
+    // }, {
+    //   id: 3,
+    //   value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+    //   endDate: '2020年10月16日',
+    //   endTime: '23:48',
+    //   isFinished: false
+    // }, {
+    //   id: 4,
+    //   value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+    //   endDate: '2020年10月16日',
+    //   endTime: '',
+    //   isFinished: false
+    // }, {
+    //   id: 5,
+    //   value: '任务6ixixi',
+    //   endDate: '',
+    //   endTime: '',
+    //   isFinished: false
+    // }, {
+    //   id: 5,
+    //   value: '任务6ixixi',
+    //   endDate: '',
+    //   endTime: '23:17',
+    //   isFinished: false
+    // }
+    ], //具体的待办事项
   },
   //处理展开收缩
   handleCollapseEvent: function(event) {
@@ -88,26 +90,21 @@ Page({
     // console.log(this.data);
   },
   onLoad: function() {
-    const{ todoList} = this.data.todoList;
-    console.log('todoList',todoList);
+    const {
+      todoList
+    } = this.data;
 
-    let todoNum = todoList.filter(item => {
+    console.log(this.data);
+    
+    let todoNum = todoList.filter(item => { //统计未完成任务数量
       return !item.isFinished;
     }).length;
 
-    let doneNum = todoList.length - todoNum;
+    let doneNum = todoList.length - todoNum;//统计已完成任务数量
 
     this.setData({
-      todo: {
-        ...todo,
-        num: todoNum
-      },
-      done: {
-        ...done,
-        num: doneNum
-      }
+      todoNum: todoNum,
+      doneNum: doneNum
     })
-
-    console.log(todoNum,doneNum);
   }
 })
