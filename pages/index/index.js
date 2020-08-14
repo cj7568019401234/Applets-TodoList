6d642b48ec1d6b727d78d9e1f2ff163e59c8e50e
liucaijun <liucaijun@jd.com>
2020-08-14 18:58:35 +0800

    fix no data error

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,43 +17,43 @@ Page({
       collapse: false,
     },
     todoList: [
-    //   {
-    //   id: 1,
-    //   value: 'hahaha todoList开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发',
-    //   endDate: '2020年10月12日',
-    //   endTime: '20:18',
-    //   isFinished: false
-    // }, {
-    //   id: 2,
-    //   value: '这是第二个任务啦',
-    //   endDate: '2020年10月12日',
-    //   endTime: '20:18',
-    //   isFinished: true
-    // }, {
-    //   id: 3,
-    //   value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-    //   endDate: '2020年10月16日',
-    //   endTime: '23:48',
-    //   isFinished: false
-    // }, {
-    //   id: 4,
-    //   value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-    //   endDate: '2020年10月16日',
-    //   endTime: '',
-    //   isFinished: false
-    // }, {
-    //   id: 5,
-    //   value: '任务6ixixi',
-    //   endDate: '',
-    //   endTime: '',
-    //   isFinished: false
-    // }, {
-    //   id: 5,
-    //   value: '任务6ixixi',
-    //   endDate: '',
-    //   endTime: '23:17',
-    //   isFinished: false
-    // }
+      {
+      id: 1,
+      value: 'hahaha todoList开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发',
+      endDate: '2020年10月12日',
+      endTime: '20:18',
+      isFinished: false
+    }, {
+      id: 2,
+      value: '这是第二个任务啦',
+      endDate: '2020年10月12日',
+      endTime: '20:18',
+      isFinished: true
+    }, {
+      id: 3,
+      value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+      endDate: '2020年10月16日',
+      endTime: '23:48',
+      isFinished: false
+    }, {
+      id: 4,
+      value: '这是第二个任务啦111~~~~~~不是的吧123445~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+      endDate: '2020年10月16日',
+      endTime: '',
+      isFinished: false
+    }, {
+      id: 5,
+      value: '任务6ixixi',
+      endDate: '',
+      endTime: '',
+      isFinished: false
+    }, {
+      id: 5,
+      value: '任务6ixixi',
+      endDate: '',
+      endTime: '23:17',
+      isFinished: false
+    }
     ], //具体的待办事项
   },
   //处理展开收缩
@@ -95,7 +95,7 @@ Page({
     } = this.data;
 
     console.log(this.data);
-    
+
     let todoNum = todoList.filter(item => { //统计未完成任务数量
       return !item.isFinished;
     }).length;
@@ -106,5 +106,7 @@ Page({
       todoNum: todoNum,
       doneNum: doneNum
     })
+
+    console.log(this.data);
   }
 })
