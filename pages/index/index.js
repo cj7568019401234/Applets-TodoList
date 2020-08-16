9994b39e37e6bb8927ba77d2a53c5107b72ac8a5
liucaijun1 <liucaijun@jd.com>
2020-08-16 23:21:43 +0800

    submit new todoList

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,13 @@ const app = getApp()
 
 Page({
   data: {
+    showModal: false,
+    value: '', //正在编辑的任务的内容
+    endDate: '', //正在编辑的任务的截止日期 
+    endTime: '', //正在编辑的任务的截止时间
     todoNum: 0,
     doneNum: 0,
+    todoDetail: '',
     todo: {
       isShowed: true, //是否展示
       spread: false, //是否点击展开
@@ -16,8 +21,7 @@ Page({
       spread: false,
       collapse: false,
     },
-    todoList: [
-      {
+    todoList: [{
       id: 1,
       value: 'hahaha todoList开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发开发',
       endDate: '2020年10月12日',
@@ -48,13 +52,12 @@ Page({
       endTime: '',
       isFinished: false
     }, {
-      id: 5,
+      id: 6,
       value: '任务6ixixi',
       endDate: '',
       endTime: '23:17',
       isFinished: false
-    }
-    ], //具体的待办事项
+    }], //具体的待办事项
   },
   //处理展开收缩
   handleCollapseEvent: function(event) {
@@ -89,24 +92,85 @@ Page({
     }
     // console.log(this.data);
   },
+
+  //展示任务编辑框
+  showModal: function() {
+    this.setData({
+      showModal: true
+    })
+  },
+
+  //取消编辑任务
+  closeModal: function() {
+    this.setData({
+      showModal: false,
+      value: '',
+      endDate: '',
+      endTime: ''
+    })
+  },
+
+  //获取文本输入框的任务内容
+  bindTextAreaBlur: function(e) {
+    this.setData({
+      value: e.detail.value
+    })
+  },
+
+  //获取任务的截止日期
+  bindDateChange: function(e) {
+    this.setData({
+      endDate: e.detail.value
+    })
+  },
+  //获取任务的截止时间
+  bindTimeChange: function(e) {
+    this.setData({
+      endTime: e.detail.value.replace('undefined', '00'),
+    })
+  },
+
+  //提交新增的待办事项
+  bindSubmit: function(e) {
+    const {
+      value,
+      endDate,
+      endTime,
+      todoList
+    } = this.data;
+
+    if (!value) return;
+    let id = new Date().toString().split('GMT')[0].replace(/\s+/g, '')
+    this.setData({
+      todoList: [
+        ...todoList,
+        {
+          id: id,
+          value: value,
+          endDate: endDate,
+          endTime: endTime,
+          isFinished: false
+        }
+      ]
+    })
+
+    this.closeModal(); //关闭弹窗
+  },
+
   onLoad: function() {
     const {
       todoList
     } = this.data;
 
-    console.log(this.data);
-
     let todoNum = todoList.filter(item => { //统计未完成任务数量
       return !item.isFinished;
     }).length;
 
-    let doneNum = todoList.length - todoNum;//统计已完成任务数量
+    let doneNum = todoList.length - todoNum; //统计已完成任务数量
 
     this.setData({
       todoNum: todoNum,
       doneNum: doneNum
     })
-
-    console.log(this.data);
   }
 })
